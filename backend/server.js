5085283aaca1ae62207122ab7683dd5604ed0ef4
deepak <sharmads2001@gmail.com>
2025-08-06 14:24:42 +0530

    update port

--- backend/server.js
+++ backend/server.js
@@ -8,8 +8,13 @@ import autoUpdateDueMembers from "./utils/autoUpdateDueMembers.js";
 dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
 
 app.use("/api/students", studentRoutes);
 
@@ -21,7 +26,7 @@ mongoose.connect(process.env.MONGO_URI)
     await autoUpdateDueMembers();
     setInterval(autoUpdateDueMembers, 24 * 60 * 60 * 1000);
 
-    app.listen(5000, () => {
+    app.listen(process.env.PORT, () => {
       console.log("Server running on port 5000");
     });
   })
